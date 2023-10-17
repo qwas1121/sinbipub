38264504ebd32235e2114c06fb06ca2d1555d7d3
qwas1121 <59353041+qwas1121@users.noreply.github.com>
2023-10-17 18:36:36 +0900

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,8 +3,8 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "bros12@",
-  database: "sinbi",
+  password: "",
+  database: "",
 });
 
 connection.connect((error) => {
